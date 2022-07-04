ab6bed1d7ebb9358ea0ff2d199b1e9beebc9d7c3
gabriel-moura <gabriel-moura11@live.com>
2022-07-04 17:50:49 -0300

    primeiro commit

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>Ola Vue!</h1>
+    <p>Meu nome é {{nome}} e trabalho como {{profissao}}</p>
+   
+</div>
+</template>
+<script>
+
+
+export default ({
+   name:'PrimeiroComponente',
+   data(){
+    return{
+        nome:"Matheus",
+        profissao:"Programador"
+    }
+   }
+})
+</script>
